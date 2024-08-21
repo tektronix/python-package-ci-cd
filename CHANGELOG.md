dc7976ce0d62aae073a6561132c6e3318385ff89
Felt, Nicholas <nicholas.felt@tektronix.com>
2024-08-21 12:51:49 -0700

    docs: Updated workflows to use pinned versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,17 @@
 # Changelog
 
+The format is based on [Keep a Changelog](https://keepachangelog.com), and this
+project adheres to [Semantic Versioning](https://semver.org).
+
+Valid subsections within a version are:
+
+- Added
+- Changed
+- Deprecated
+- Removed
+- Fixed
+- Security
+
 ---
 
 ## Unreleased
@@ -8,12 +20,14 @@ Things to be included in the next release go here.
 
 ### Added
 
-- Something will be added
-
----
-
-## v0.0.1
-
-### Added
-
-- Something was done here in the past
+- Added a reusable workflow to check the Python API for breaking changes: [`check-api-for-breaking-changes.yml`](./workflows/check-api-for-breaking-changes.md)
+- Added a reusable workflow to perform CodeQL analysis: [`codeql-analysis.yml`](./workflows/codeql-analysis.md)
+- Added a reusable workflow to enforce Open-Source community standards: [`enforce-community-standards.yml`](./workflows/enforce-community-standards.md)
+- Added a reusable workflow to build a Python package: [`package-build.yml`](./workflows/package-build.md)
+- Added a reusable workflow to upload a Python package to TestPyPI: [`package-testpypi.yml`](./workflows/package-testpypi.md)
+- Added a reusable workflow to publish API comparison results as a Pull Request comment: [`publish-api-comparison.yml`](./workflows/publish-api-comparison.md)
+- Added a reusable workflow to publish test results as a Pull Request comment: [`publish-test-results.yml`](./workflows/publish-test-results.md)
+- Added a reusable workflow to create a Software Bill of Materials (SBOM) and scan it: [`sbom-scan.yml`](./workflows/sbom-scan.md)
+- Added a reusable workflow to run tests and linting against Python package code: [`test-code.yml`](./workflows/test-code.md)
+- Added a reusable workflow to run documentation builds and tests for a Python package: [`test-docs.yml`](./workflows/test-docs.md)
+- Added a reusable workflow to update Python and `pre-commit` dependencies: [`update-python-and-pre-commit-dependencies.yml`](./workflows/update-python-and-pre-commit-dependencies.md)
---
+++ scripts/bump_version_in_files.py
@@ -0,0 +1,61 @@
+"""This script will bump the version of the GitHub Actions to the newest version.
+
+This script is intended to be run as a part of the release process in the `python-semantic-release`
+`build-command`.
+"""
+
+from __future__ import annotations
+
+import os
+import re
+
+from pathlib import Path
+
+DIRECTORIES_TO_SEARCH = [".github", "workflows", "actions"]
+FILES_TO_UPDATE = [Path("README.md")]
+GITHUB_WORKFLOW_AND_ACTION_REGEX = re.compile(
+    r"(uses: tektronix/python-package-ci-cd/.*?@v)\d+\.\d+\.\d+"
+)
+
+
+def get_file_paths(directory_list: list[str]) -> list[Path]:
+    """Get a list of file paths from the given directories.
+
+    Args:
+        directory_list: A list of directories to search for files in.
+    """
+    file_paths: list[Path] = []
+    for directory in directory_list:
+        for dirpath, _, filenames in os.walk(directory):
+            for filename in filenames:
+                file_paths.append(Path(dirpath) / Path(filename))  # noqa: PERF401
+    return file_paths
+
+
+def update_github_actions_version(filepath: Path, incoming_version: str) -> None:
+    """Update the version of the GitHub Actions to the incoming version.
+
+    Args:
+        filepath: The path to the file to update.
+        incoming_version: The version to update the file to
+    """
+    file_content = filepath.read_text()
+    # Check if there's a match before replacing
+    if GITHUB_WORKFLOW_AND_ACTION_REGEX.search(file_content):
+        # Replace the version numbers with the new version
+        updated_content = GITHUB_WORKFLOW_AND_ACTION_REGEX.sub(
+            rf"\1@v{incoming_version}", file_content
+        )
+        print(f'Bumping version in "{filepath}" to', incoming_version)
+        filepath.write_text(updated_content + "\n")
+    else:
+        print(f'No GitHub Workflow/Action usage found in "{filepath}", skipping update.')
+
+
+if __name__ == "__main__":
+    if not (new_version := os.getenv("NEW_VERSION")):
+        msg = "NEW_VERSION environment variable is not set"
+        raise SystemExit(msg)
+
+    for file_path in get_file_paths(DIRECTORIES_TO_SEARCH) + FILES_TO_UPDATE:
+        update_github_actions_version(file_path, new_version)
