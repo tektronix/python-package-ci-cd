68f78135f65164c5728395ee4c8bf4709e5f1809
Nicholas Felt <nicholas.felt@tektronix.com>
2025-04-10 12:07:07 -0700

    Revert "refactor: Simplify the formatCopyText javascript function to increase the codefactor score"
    This reverts commit ece54e72c0cef10e7eb394dc8e86ef3c13aba242.

--- doc_config/_static/js/copybutton.js
+++ doc_config/_static/js/copybutton.js
@@ -158,44 +158,56 @@ function filterText(target, exclude) {
 
 // Callback when a copy button is clicked. Will be passed the node that was clicked
 // should then grab the text and replace pieces of text that shouldn't be used in output
-function formatCopyText(
-  textContent,
-  copybuttonPromptText,
-  isRegexp = false,
-  onlyCopyPromptLines = true,
-  removePrompts = true,
-  copyEmptyLines = true,
-  lineContinuationChar = "",
-  hereDocDelim = ""
-) {
-  const useLineCont = !!lineContinuationChar;
-  const useHereDoc = !!hereDocDelim;
+function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onlyCopyPromptLines = true, removePrompts = true, copyEmptyLines = true, lineContinuationChar = "", hereDocDelim = "") {
+    let regexp;
+    let match;
 
-  const regexp = new RegExp(
-    `^(${isRegexp ? copybuttonPromptText : escapeRegExp(copybuttonPromptText)})(.*)`
-  );
+    // Do we check for line continuation characters and "HERE-documents"?
+    let useLineCont = !!lineContinuationChar
+    let useHereDoc = !!hereDocDelim
 
-  const outputLines = [];
-  let isHereDocActive = false;
-
-  for (const line of textContent.split("\n")) {
-    const match = line.match(regexp);
-    const isPromptLine = match !== null;
-
-    if (isPromptLine || isHereDocActive || (useLineCont && line.endsWith(lineContinuationChar))) {
-      if (removePrompts && isPromptLine) {
-        outputLines.push(match[2]);
-      } else {
-        outputLines.push(line);
-      }
-      isHereDocActive = useHereDoc && line.includes(hereDocDelim) ? !isHereDocActive : isHereDocActive;
-    } else if (!onlyCopyPromptLines || (copyEmptyLines && line.trim() === "")) {
-      outputLines.push(line);
+    // create regexp to capture prompt and remaining line
+    if (isRegexp) {
+        regexp = new RegExp('^(' + copybuttonPromptText + ')(.*)')
+    } else {
+        regexp = new RegExp('^(' + escapeRegExp(copybuttonPromptText) + ')(.*)')
     }
-  }
 
-  // Remove trailing newline to avoid auto-running when pasting
-  return outputLines.join("\n").replace(/\n$/, "");
+    const outputLines = [];
+    let promptFound = false;
+    let gotLineCont = false;
+    let gotHereDoc = false;
+    const lineGotPrompt = [];
+    for (const line of textContent.split('\n')) {
+        match = line.match(regexp)
+        if (match || gotLineCont || gotHereDoc) {
+            promptFound = regexp.test(line)
+            lineGotPrompt.push(promptFound)
+            if (removePrompts && promptFound) {
+                outputLines.push(match[2])
+            } else {
+                outputLines.push(line)
+            }
+            gotLineCont = line.endsWith(lineContinuationChar) & useLineCont
+            if (line.includes(hereDocDelim) & useHereDoc)
+                gotHereDoc = !gotHereDoc
+        } else if (!onlyCopyPromptLines) {
+            outputLines.push(line)
+        } else if (copyEmptyLines && line.trim() === '') {
+            outputLines.push(line)
+        }
+    }
+
+    // If no lines with the prompt were found then just use original lines
+    if (lineGotPrompt.some(v => v === true)) {
+        textContent = outputLines.join('\n');
+    }
+
+    // Remove a trailing newline to avoid auto-running when pasting
+    if (textContent.endsWith("\n")) {
+        textContent = textContent.slice(0, -1)
+    }
+    return textContent
 }
 
 
